be4c7b2b19d6e4723b235e3f282ba1c4f916e5ac
UNIQUE PANGHAL <panghalunique@gmail.com>
2024-04-03 18:06:03 +0530

    Updated code to include new features

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,22 @@
 const { Sequelize } = require('sequelize');
 require("dotenv").config();
 
-const sequelize = new Sequelize('heroproject', 'admin', 'YoSR7o67IbmgQh3dXCyD083a9EmSv5j0', {
-  host: 'dpg-cmihk07109ks739luah0-a.singapore-postgres.render.com',
+// const sequelize = new Sequelize('heroproject', 'admin', 'YoSR7o67IbmgQh3dXCyD083a9EmSv5j0', {
+//   host: 'dpg-cmihk07109ks739luah0-a.singapore-postgres.render.com',
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: false,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
+// const sequelize = new Sequelize('heroproject', 'admin', 'rootuser', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+const sequelize = new Sequelize('programdashboard', 'admin', '0t1sDFaMMDDjNll5jiF9l1zSFm5iWacv', {
+  host: 'dpg-co6k6i8l6cac73aagab0-a.singapore-postgres.render.com',
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
@@ -11,11 +25,8 @@ const sequelize = new Sequelize('heroproject', 'admin', 'YoSR7o67IbmgQh3dXCyD083
     },
   },
 });
-// const sequelize = new Sequelize(process.env.DB_URL, {
-//   dialect: "postgres",
-//   logging: false,
-  
 
 // });
+///
 
 module.exports = sequelize;
